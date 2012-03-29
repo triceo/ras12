4c32449d6e6e5695bc9f4b40b2ac367389f41488
Lukáš Petrovický <lpetrovi@redhat.com>
2012-03-29 10:43:43 +0200

    Initial project structure

---
+++ src/main/java/org/drools/planner/examples/ras2012/App.java
@@ -0,0 +1,13 @@
+package org.drools.planner.examples.ras2012;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
