c343b98c89aeeb6cf17e31522272bcc59d07736e
Lukáš Petrovický <lukas@petrovicky.net>
2012-05-22 20:21:53 +0200

    Don't benchmark SA other than 4.

--- ras2012-benchmark/src/main/java/org/drools/planner/examples/ras2012/Benchmark.java
+++ ras2012-benchmark/src/main/java/org/drools/planner/examples/ras2012/Benchmark.java
@@ -17,7 +17,7 @@ public class Benchmark {
     private static Map getTemplateData() {
         int[] hardSA = new int[] { 0, 5, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100 };
         int[] softSA = new int[] { 0, 5, 10, 50, 100, 500, 1000, 5000, 10000 };
-        int[] minimalSA = new int[] { 4, 8, 16 };
+        int[] minimalSA = new int[] { 4 };
         Collection<int[]> SAs = new LinkedList<int[]>();
         for (int hard : hardSA) {
             for (int soft : softSA) {
