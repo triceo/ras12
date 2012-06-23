37a152c29ca18ce9d37b54aef2275802a21141c0
Lukáš Petrovický <lukas@petrovicky.net>
2012-06-23 17:33:10 +0200

    Unify solution names in TeX files.

--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/util/SolutionIO.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/util/SolutionIO.java
@@ -646,7 +646,7 @@ public class SolutionIO {
     public void writeTex(final ProblemSolution solution, final File outputSolutionFile) {
         try {
             final Map map = this.prepareTexData(solution);
-            map.put("id", outputSolutionFile.getName());
+            map.put("id", solution.getName().replaceAll("\\Q \\E", ""));
             this.freemarker.getTemplate("schedule.tex.ftl").process(map,
                     new FileWriter(outputSolutionFile));
         } catch (final TemplateException e) {
