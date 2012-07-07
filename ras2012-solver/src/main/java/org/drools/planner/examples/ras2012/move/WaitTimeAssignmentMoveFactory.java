8ae1353c0fbbdaa8b325a18d585004320b6b4ba7
Lukáš Petrovický <lukas@petrovicky.net>
2012-07-07 21:05:06 +0200

    Reduce the number of moves.

--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
@@ -1,10 +1,8 @@
 package org.drools.planner.examples.ras2012.move;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 import org.drools.planner.core.move.Move;
@@ -26,14 +24,14 @@ public class WaitTimeAssignmentMoveFactory extends AbstractMoveFactory {
      * Numbers from 0 to this will all become wait times. This is done so that the algorithm has enough space for fine-tuning
      * the results.
      */
-    private static final int    ALL_FIRST_X = 10;
+    private static final int    ALL_FIRST_X = 5;
 
     /**
      * Specifies what change there will be between two consecutive wait times. Please keep it between 0 and 1, both exclusive.
      * 
      * The current value has been carefully benchmarked against many other values and found to bring the best results.
      */
-    private static final float  DECREASE_TO = 4.0f / 5.0f;
+    private static final float  DECREASE_TO = 7.0f / 8.0f;
 
     private static List<WaitTime> getAllowedWaitTimes(final long horizon) {
         final List<WaitTime> waitTimes = new LinkedList<WaitTime>();
@@ -42,10 +40,9 @@ public class WaitTimeAssignmentMoveFactory extends AbstractMoveFactory {
             waitTimes.add(WaitTime.getWaitTime(waitTime));
             waitTime = Math.round(waitTime * WaitTimeAssignmentMoveFactory.DECREASE_TO);
         }
-        for (int i = WaitTimeAssignmentMoveFactory.ALL_FIRST_X; i > 0; i--) {
-            waitTimes.add(WaitTime.getWaitTime(i));
+        for (long i = Math.min(horizon, WaitTimeAssignmentMoveFactory.ALL_FIRST_X); i > 0; i--) {
+            waitTimes.add(WaitTime.getWaitTime((int) i));
         }
-        waitTimes.add(null);
         WaitTimeAssignmentMoveFactory.logger
                 .info("Minutes of wait time will multiply by {}, starting with {} and until {} is reached, from where they will decrease by one.",
                         new Object[] { WaitTimeAssignmentMoveFactory.DECREASE_TO, horizon,
@@ -55,28 +52,44 @@ public class WaitTimeAssignmentMoveFactory extends AbstractMoveFactory {
         return waitTimes;
     }
 
-    private final Map<Long, List<WaitTime>> waitTimes = new HashMap<Long, List<WaitTime>>();
-
     @Override
     public List<Move> createMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
         final ProblemSolution sol = (ProblemSolution) solution;
-        final long horizon = sol.getPlanningHorizon(TimeUnit.MINUTES);
-        // enumerate every possible wait time value
-        if (!this.waitTimes.containsKey(horizon)) {
-            this.waitTimes.put(horizon, WaitTimeAssignmentMoveFactory.getAllowedWaitTimes(horizon));
-        }
+        final long horizon = sol.getPlanningHorizon(TimeUnit.MILLISECONDS);
         // TODO estimate maximum necessary wait time from the longest arc and slowest train
         final List<Move> moves = new ArrayList<Move>();
         for (final ItineraryAssignment ia : sol.getAssignments()) {
             // when train entered X minutes after start of world, don't generate wait times to cover those X minutes.
-            final long maxPlanningHorizon = horizon - ia.getTrain().getEntryTime(TimeUnit.MINUTES);
             for (final Node waitPoint : ia.getRoute().getProgression().getWaitPoints()) {
-                for (final WaitTime wt : this.waitTimes.get(horizon)) {
-                    if (wt == null || wt.getWaitFor(TimeUnit.MINUTES) <= maxPlanningHorizon) {
-                        moves.add(new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(),
-                                waitPoint, wt));
-                    }
+                if (!ia.getItinerary().hasNode(waitPoint)) {
+                    continue;
                 }
+                WaitTime existingWaitTime = ia.getItinerary().getWaitTime(waitPoint);
+                long currentArrival = ia.getItinerary().getArrivalTime(waitPoint);
+                long actualHorizon = 0;
+                if (currentArrival > horizon) {
+                    actualHorizon = existingWaitTime == null ? 0 : existingWaitTime
+                            .getWaitFor(TimeUnit.MILLISECONDS);
+                } else {
+                    // otherwise only accept wait times that won't cause the train going over the horizon much
+                    actualHorizon = horizon
+                            - currentArrival
+                            + (existingWaitTime == null ? 0 : existingWaitTime
+                                    .getWaitFor(TimeUnit.MILLISECONDS))
+                            + TimeUnit.MINUTES.toMillis(60);
+                }
+                actualHorizon = Math.max(actualHorizon - 1, 0);
+                actualHorizon = TimeUnit.MILLISECONDS.toMinutes(actualHorizon);
+                for (final WaitTime wt : WaitTimeAssignmentMoveFactory
+                        .getAllowedWaitTimes(actualHorizon)) {
+                    if (existingWaitTime == wt) {
+                        // there already is such wait time; no need to create the move
+                        continue;
+                    }
+                    moves.add(new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(), waitPoint,
+                            wt));
+                }
+                moves.add(new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(), waitPoint, null));
             }
         }
         return moves;
