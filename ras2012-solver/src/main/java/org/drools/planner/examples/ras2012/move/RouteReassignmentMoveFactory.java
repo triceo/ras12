a12f4b9e8903fd1ae95bdceed9e2464598464e2f
Lukáš Petrovický <lpetrovi@redhat.com>
2012-07-28 09:40:57 +0200

    Upgrade to latest planner.

--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/RouteReassignmentMoveFactory.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/RouteReassignmentMoveFactory.java
@@ -3,17 +3,17 @@ package org.drools.planner.examples.ras2012.move;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.drools.planner.core.heuristic.selector.move.factory.MoveListFactory;
 import org.drools.planner.core.move.Move;
-import org.drools.planner.core.move.factory.CachedMoveFactory;
 import org.drools.planner.core.solution.Solution;
 import org.drools.planner.examples.ras2012.ProblemSolution;
 import org.drools.planner.examples.ras2012.model.Route;
 import org.drools.planner.examples.ras2012.model.Train;
 
-public class RouteReassignmentMoveFactory extends CachedMoveFactory {
+public class RouteReassignmentMoveFactory implements MoveListFactory {
 
     @Override
-    public List<Move> createCachedMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
+    public List<Move> createMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
         final List<Move> moves = new ArrayList<Move>();
         final ProblemSolution sol = (ProblemSolution) solution;
         for (final Train t : sol.getTrains()) {
--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
@@ -3,14 +3,14 @@ package org.drools.planner.examples.ras2012.move;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.drools.planner.core.heuristic.selector.move.factory.MoveListFactory;
 import org.drools.planner.core.move.Move;
-import org.drools.planner.core.move.factory.AbstractMoveFactory;
 import org.drools.planner.core.solution.Solution;
 import org.drools.planner.examples.ras2012.ProblemSolution;
 import org.drools.planner.examples.ras2012.model.ItineraryAssignment;
 import org.drools.planner.examples.ras2012.model.WaitTimeAssignment;
 
-public class WaitTimeAssignmentMoveFactory extends AbstractMoveFactory {
+public class WaitTimeAssignmentMoveFactory implements MoveListFactory {
 
     @Override
     public List<Move> createMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
