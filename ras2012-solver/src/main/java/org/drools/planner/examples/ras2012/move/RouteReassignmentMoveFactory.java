a06d3bf04d99c4e7aecaac956b0f29fdc9d51fac
Lukáš Petrovický <lpetrovi@redhat.com>
2012-08-09 12:58:46 +0200

    Convert MoveListFactory to MoveIteratorFactory.

--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/RouteReassignmentMoveFactory.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/RouteReassignmentMoveFactory.java
@@ -1,27 +1,118 @@
 package org.drools.planner.examples.ras2012.move;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
-import org.drools.planner.core.heuristic.selector.move.factory.MoveListFactory;
+import org.apache.commons.lang3.tuple.Pair;
+import org.drools.planner.core.heuristic.selector.move.factory.MoveIteratorFactory;
 import org.drools.planner.core.move.Move;
-import org.drools.planner.core.solution.Solution;
+import org.drools.planner.core.score.director.ScoreDirector;
 import org.drools.planner.examples.ras2012.ProblemSolution;
 import org.drools.planner.examples.ras2012.model.Route;
 import org.drools.planner.examples.ras2012.model.Train;
 
-public class RouteReassignmentMoveFactory implements MoveListFactory {
+public class RouteReassignmentMoveFactory implements MoveIteratorFactory {
 
-    @Override
-    public List<Move> createMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
-        final List<Move> moves = new ArrayList<Move>();
-        final ProblemSolution sol = (ProblemSolution) solution;
-        for (final Train t : sol.getTrains()) {
-            for (final Route r : sol.getTerritory().getRoutes(t)) {
-                moves.add(new RouteReassignmentMove(t, r));
+    private static final class RandomRouteReassignmentMoveIterator implements Iterator<Move> {
+
+        private final List<Pair<Train, Route>> pairs       = new ArrayList<Pair<Train, Route>>();
+        private final Random                   random;
+        private final Set<Integer>             usedIndices = new TreeSet<Integer>();
+
+        public RandomRouteReassignmentMoveIterator(final ProblemSolution solution,
+                final Random random) {
+            for (final Train t : solution.getTrains()) {
+                for (final Route r : solution.getTerritory().getRoutes(t)) {
+                    this.pairs.add(Pair.of(t, r));
+                }
+            }
+            this.random = random;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.usedIndices.size() < this.pairs.size();
+        }
+
+        @Override
+        public Move next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int index = -1;
+            do {
+                index = this.random.nextInt(this.pairs.size());
+            } while (this.usedIndices.contains(index));
+            this.usedIndices.add(index);
+            final Pair<Train, Route> pair = this.pairs.get(index);
+            return new RouteReassignmentMove(pair.getLeft(), pair.getRight());
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private static final class RouteReassignmentMoveIterator implements Iterator<Move> {
+
+        private final List<Pair<Train, Route>> pairs        = new ArrayList<Pair<Train, Route>>();
+        private int                            currentIndex = 0;
+
+        public RouteReassignmentMoveIterator(final ProblemSolution solution) {
+            for (final Train t : solution.getTrains()) {
+                for (final Route r : solution.getTerritory().getRoutes(t)) {
+                    this.pairs.add(Pair.of(t, r));
+                }
             }
         }
-        return moves;
+
+        @Override
+        public boolean hasNext() {
+            return this.currentIndex < this.pairs.size() - 1;
+        }
+
+        @Override
+        public Move next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            final Pair<Train, Route> pair = this.pairs.get(this.currentIndex++);
+            return new RouteReassignmentMove(pair.getLeft(), pair.getRight());
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    @Override
+    public Iterator<Move> createOriginalMoveIterator(final ScoreDirector arg0) {
+        return new RouteReassignmentMoveIterator((ProblemSolution) arg0.getWorkingSolution());
+    }
+
+    @Override
+    public Iterator<Move> createRandomMoveIterator(final ScoreDirector arg0, final Random arg1) {
+        return new RandomRouteReassignmentMoveIterator((ProblemSolution) arg0.getWorkingSolution(),
+                arg1);
+    }
+
+    @Override
+    public long getSize(final ScoreDirector arg0) {
+        final ProblemSolution sol = (ProblemSolution) arg0.getWorkingSolution();
+        long size = 0;
+        for (final Train t : sol.getTrains()) {
+            size += sol.getTerritory().getRoutes(t).size();
+        }
+        return size;
     }
 
 }
--- ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
+++ ras2012-solver/src/main/java/org/drools/planner/examples/ras2012/move/WaitTimeAssignmentMoveFactory.java
@@ -1,28 +1,124 @@
 package org.drools.planner.examples.ras2012.move;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
 
-import org.drools.planner.core.heuristic.selector.move.factory.MoveListFactory;
+import org.apache.commons.lang3.tuple.Pair;
+import org.drools.planner.core.heuristic.selector.move.factory.MoveIteratorFactory;
 import org.drools.planner.core.move.Move;
-import org.drools.planner.core.solution.Solution;
+import org.drools.planner.core.score.director.ScoreDirector;
 import org.drools.planner.examples.ras2012.ProblemSolution;
 import org.drools.planner.examples.ras2012.model.ItineraryAssignment;
 import org.drools.planner.examples.ras2012.model.WaitTimeAssignment;
 
-public class WaitTimeAssignmentMoveFactory implements MoveListFactory {
+public class WaitTimeAssignmentMoveFactory implements MoveIteratorFactory {
 
-    @Override
-    public List<Move> createMoveList(@SuppressWarnings("rawtypes") final Solution solution) {
-        final ProblemSolution sol = (ProblemSolution) solution;
-        // TODO estimate maximum necessary wait time from the longest arc and slowest train
-        final List<Move> moves = new ArrayList<Move>();
-        for (final ItineraryAssignment ia : sol.getAssignments()) {
-            for (final WaitTimeAssignment wt : ia.getWaitTimeAssignments()) {
-                moves.add(new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(), wt.getNode(), wt
-                        .getWaitTime()));
+    private static final class RandomWaitTimeAssignmentMoveIterator implements Iterator<Move> {
+
+        private final List<Pair<ItineraryAssignment, WaitTimeAssignment>> pairs       = new ArrayList<Pair<ItineraryAssignment, WaitTimeAssignment>>();
+        private final Random                                              random;
+        private final Set<Integer>                                        usedIndices = new TreeSet<Integer>();
+
+        public RandomWaitTimeAssignmentMoveIterator(final ProblemSolution solution,
+                final Random random) {
+            for (final ItineraryAssignment ia : solution.getAssignments()) {
+                for (final WaitTimeAssignment wt : ia.getWaitTimeAssignments()) {
+                    this.pairs.add(Pair.of(ia, wt));
+                }
+            }
+            this.random = random;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return this.usedIndices.size() < this.pairs.size();
+        }
+
+        @Override
+        public Move next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            int index = -1;
+            do {
+                index = this.random.nextInt(this.pairs.size());
+            } while (this.usedIndices.contains(index));
+            this.usedIndices.add(index);
+            final Pair<ItineraryAssignment, WaitTimeAssignment> pair = this.pairs.get(index);
+            final ItineraryAssignment ia = pair.getLeft();
+            final WaitTimeAssignment wta = pair.getRight();
+            return new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(), wta.getNode(),
+                    wta.getWaitTime());
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    private static final class WaitTimeAssignmentMoveIterator implements Iterator<Move> {
+
+        private final List<Pair<ItineraryAssignment, WaitTimeAssignment>> pairs        = new ArrayList<Pair<ItineraryAssignment, WaitTimeAssignment>>();
+        private int                                                       currentIndex = 0;
+
+        public WaitTimeAssignmentMoveIterator(final ProblemSolution solution) {
+            for (final ItineraryAssignment ia : solution.getAssignments()) {
+                for (final WaitTimeAssignment wt : ia.getWaitTimeAssignments()) {
+                    this.pairs.add(Pair.of(ia, wt));
+                }
             }
         }
-        return moves;
+
+        @Override
+        public boolean hasNext() {
+            return this.currentIndex < this.pairs.size() - 1;
+        }
+
+        @Override
+        public Move next() {
+            if (!this.hasNext()) {
+                throw new NoSuchElementException();
+            }
+            final Pair<ItineraryAssignment, WaitTimeAssignment> pair = this.pairs
+                    .get(this.currentIndex++);
+            final ItineraryAssignment ia = pair.getLeft();
+            final WaitTimeAssignment wta = pair.getRight();
+            return new WaitTimeAssignmentMove(ia.getTrain(), ia.getRoute(), wta.getNode(),
+                    wta.getWaitTime());
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    @Override
+    public Iterator<Move> createOriginalMoveIterator(final ScoreDirector arg0) {
+        return new WaitTimeAssignmentMoveIterator((ProblemSolution) arg0.getWorkingSolution());
+    }
+
+    @Override
+    public Iterator<Move> createRandomMoveIterator(final ScoreDirector arg0, final Random arg1) {
+        return new RandomWaitTimeAssignmentMoveIterator(
+                (ProblemSolution) arg0.getWorkingSolution(), arg1);
+    }
+
+    @Override
+    public long getSize(final ScoreDirector arg0) {
+        final ProblemSolution sol = (ProblemSolution) arg0.getWorkingSolution();
+        long size = 0;
+        for (final ItineraryAssignment ia : sol.getAssignments()) {
+            size += ia.getWaitTimeAssignments().size();
+        }
+        return size;
     }
 }
